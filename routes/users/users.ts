66929817c16dd2b2c75d64e27a2f494333edc393
mgregerson <mgregerson4321@gmail.com>
2023-09-11 11:12:09 -0400

    Added route to get tests for a user by difficulty

--- routes/users/users.ts
+++ routes/users/users.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { prisma } from "../../lib/prisma";
+import { Difficulty } from "@prisma/client";
 
 export const usersRouter = Router();
 
@@ -22,6 +23,44 @@ usersRouter.get("/:id", async (req, res) => {
     res.json(user);
   } catch (error) {
     res.json({ error });
+  }
+});
+
+usersRouter.get("/:id/scores/:difficulty", async (req, res) => {
+  try {
+    const userId = Number(req.params.id);
+    const difficulty = req.params.difficulty;
+
+    // Ensure that the difficulty level is valid based on your application's requirements
+    // You may want to add additional validation logic here
+
+    // First, fetch the user by their ID
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found." });
+    }
+
+    // Now, you can fetch all scores for the user that match the specified difficulty
+    const userScores = await prisma.score.findMany({
+      where: {
+        userId,
+        typingTest: {
+          difficulty: difficulty as Difficulty,
+        },
+      },
+    });
+
+    res.json(userScores);
+  } catch (error) {
+    console.error("Error fetching user scores:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching user scores." });
   }
 });
 
